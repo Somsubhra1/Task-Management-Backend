e03837904b3ab03a169b89192c6d69c739fe0ee6
Somsubhra Das <das.somsubhra1@gmail.com>
2020-04-30 22:54:50 +0530

    createTask Controller created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
 import { Task } from './tasks.model';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -9,5 +9,17 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    // createTask(@Body() body) {
+    //     // @Body() decorator is used to get the values from the request body and store in body variable
+    //     console.log(body);
+    // }
+    createTask(
+        @Body('title') title: String,
+        @Body('description') description: String,
+    ): Task {
+        return this.tasksService.createTask(title, description);
     }
 }

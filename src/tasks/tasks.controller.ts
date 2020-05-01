31dfc59b9f72daf0c13bc3e71749652e173d70d5
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-01 16:52:17 +0530

    Added status validation pipe

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,6 +14,7 @@ import {
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -55,7 +56,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: String,
-        @Body('status') status: TaskStatus,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
     ): Task {
         return this.tasksService.updateTaskStatus(id, status);
     }

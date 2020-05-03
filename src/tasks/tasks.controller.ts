50f608ca9d61b0095cb3b45ab7622b753ae510a3
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-03 13:07:12 +0530

    Code refactoring complete

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,4 @@
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task, TaskStatus } from './tasks.model';
 import {
     Controller,
     Get,
@@ -16,48 +15,52 @@ import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
+// @Controller('tasks')
+// export class TasksController {
+//     constructor(private tasksService: TasksService) {}
+
+//     @Get()
+//     getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+//         if (Object.keys(filterDto).length) {
+//             return this.tasksService.getTaskWithFilters(filterDto);
+//         } else {
+//             return this.tasksService.getAllTasks();
+//         }
+//     }
+
+//     @Get('/:id')
+//     getTaskById(@Param('id') id: String): Task {
+//         return this.tasksService.getTaskById(id);
+//     }
+
+//     @Post()
+//     // createTask(@Body() body) {
+//     //     // @Body() decorator is used to get the values from the request body and store in body variable
+//     //     console.log(body);
+//     // }
+//     @UsePipes(ValidationPipe) // can use @Body(ValidationPipe) also
+//     createTask(
+//         // @Body('title') title: String,
+//         // @Body('description') description: String,
+//         @Body() createTaskDto: CreateTaskDto,
+//     ): Task {
+//         return this.tasksService.createTask(createTaskDto);
+//     }
+
+//     @Delete('/:id')
+//     deleteTask(@Param('id') id: String): void {
+//         this.tasksService.deleteTask(id);
+//     }
+
+//     @Patch('/:id/status')
+//     updateTaskStatus(
+//         @Param('id') id: String,
+//         @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+//     ): Task {
+//         return this.tasksService.updateTaskStatus(id, status);
+//     }
+// }
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
-
-    @Get()
-    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-        if (Object.keys(filterDto).length) {
-            return this.tasksService.getTaskWithFilters(filterDto);
-        } else {
-            return this.tasksService.getAllTasks();
-        }
-    }
-
-    @Get('/:id')
-    getTaskById(@Param('id') id: String): Task {
-        return this.tasksService.getTaskById(id);
-    }
-
-    @Post()
-    // createTask(@Body() body) {
-    //     // @Body() decorator is used to get the values from the request body and store in body variable
-    //     console.log(body);
-    // }
-    @UsePipes(ValidationPipe) // can use @Body(ValidationPipe) also
-    createTask(
-        // @Body('title') title: String,
-        // @Body('description') description: String,
-        @Body() createTaskDto: CreateTaskDto,
-    ): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
-
-    @Delete('/:id')
-    deleteTask(@Param('id') id: String): void {
-        this.tasksService.deleteTask(id);
-    }
-
-    @Patch('/:id/status')
-    updateTaskStatus(
-        @Param('id') id: String,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-    ): Task {
-        return this.tasksService.updateTaskStatus(id, status);
-    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,62 +1,64 @@
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task, TaskStatus } from './tasks.model';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v1 as uuidv1 } from 'uuid';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
+// @Injectable()
+// export class TasksService {
+//     private tasks: Task[] = [];
+
+//     getAllTasks(): Task[] {
+//         return this.tasks;
+//     }
+
+//     getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
+//         const { status, search } = filterDto;
+//         let tasks = this.getAllTasks();
+//         if (status) {
+//             tasks = tasks.filter(task => task.status === status);
+//         }
+//         if (search) {
+//             tasks = tasks.filter(
+//                 task =>
+//                     task.title.includes(String(search)) ||
+//                     task.description.includes(String(search)),
+//             );
+//         }
+
+//         return tasks;
+//     }
+
+//     getTaskById(id: String): Task {
+//         const found = this.tasks.find(task => task.id === id);
+//         if (!found) {
+//             throw new NotFoundException(`Task with id ${id} not found!`);
+//         }
+//         return found;
+//     }
+
+//     createTask(createTaskDto: CreateTaskDto): Task {
+//         const { title, description } = createTaskDto;
+//         const task: Task = {
+//             id: uuidv1(),
+//             title,
+//             description,
+//             status: TaskStatus.OPEN,
+//         };
+//         this.tasks.push(task);
+//         return task;
+//     }
+
+//     deleteTask(id: String): void {
+//         const found = this.getTaskById(id);
+//         this.tasks = this.tasks.filter(task => task.id !== found.id);
+//     }
+
+//     updateTaskStatus(id: String, status: TaskStatus): Task {
+//         const task = this.getTaskById(id);
+//         task.status = status;
+//         return task;
+//     }
+// }
+
 @Injectable()
-export class TasksService {
-    private tasks: Task[] = [];
-
-    getAllTasks(): Task[] {
-        return this.tasks;
-    }
-
-    getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
-        const { status, search } = filterDto;
-        let tasks = this.getAllTasks();
-        if (status) {
-            tasks = tasks.filter(task => task.status === status);
-        }
-        if (search) {
-            tasks = tasks.filter(
-                task =>
-                    task.title.includes(String(search)) ||
-                    task.description.includes(String(search)),
-            );
-        }
-
-        return tasks;
-    }
-
-    getTaskById(id: String): Task {
-        const found = this.tasks.find(task => task.id === id);
-        if (!found) {
-            throw new NotFoundException(`Task with id ${id} not found!`);
-        }
-        return found;
-    }
-
-    createTask(createTaskDto: CreateTaskDto): Task {
-        const { title, description } = createTaskDto;
-        const task: Task = {
-            id: uuidv1(),
-            title,
-            description,
-            status: TaskStatus.OPEN,
-        };
-        this.tasks.push(task);
-        return task;
-    }
-
-    deleteTask(id: String): void {
-        const found = this.getTaskById(id);
-        this.tasks = this.tasks.filter(task => task.id !== found.id);
-    }
-
-    updateTaskStatus(id: String, status: TaskStatus): Task {
-        const task = this.getTaskById(id);
-        task.status = status;
-        return task;
-    }
-}
+export class TasksService {}

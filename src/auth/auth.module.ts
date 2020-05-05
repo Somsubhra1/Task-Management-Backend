23f326ce20d888f76be903a7f55bf57aaf924f81
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-05 12:14:17 +0530

    User repository created

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [TypeOrmModule.forFeature([UserRepository])],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}

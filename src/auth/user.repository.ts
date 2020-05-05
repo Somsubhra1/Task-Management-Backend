7db5e8125b7e4bbf98ba76d16ff15135cce0fc26
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-05 21:57:53 +0530

    Signup password hashing implemented

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,21 @@ import {
     ConflictException,
     InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
+
+        const salt = await bcrypt.genSalt();
+        // console.log(salt);
+
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.password = await this.hashPassword(password, salt);
+        user.salt = salt;
+        // console.log(user.password);
 
         try {
             await user.save();
@@ -23,5 +30,12 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(
+        password: string,
+        salt: string,
+    ): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }

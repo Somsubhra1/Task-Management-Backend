05529611561c83537fdbb5ad839d26102302a2f7
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-05 23:11:10 +0530

    jwt accesstoken created on signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,33 @@
+import { JwtPayload } from './jwt-payload.interface';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
+        private jwtService: JwtService,
     ) {}
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
     }
-    async signIn(authCredentialsDto: AuthCredentialsDto) {
+    async signIn(
+        authCredentialsDto: AuthCredentialsDto,
+    ): Promise<{ accessToken: string }> {
         const username = await this.userRepository.validateUserPassword(
             authCredentialsDto,
         );
         if (!username) {
             throw new UnauthorizedException('Incorrect credentials');
         }
+
+        const payload: JwtPayload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+        return { accessToken };
     }
 }

7e3a7611578170f4025fc5aa3e2fa0e8beb080f0
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-07 16:49:57 +0530

    Guarded all routes on task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,12 +11,14 @@ import {
     UsePipes,
     ValidationPipe,
     ParseIntPipe,
+    UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
 
 // @Controller('tasks')
 // export class TasksController {
@@ -64,6 +66,7 @@ import { TaskStatus } from './task-status.enum';
 //     }
 // }
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 

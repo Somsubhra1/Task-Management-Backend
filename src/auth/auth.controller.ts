adc21d313be13704c24f966d61553ab8f77bba16
Somsubhra Das <das.somsubhra1@gmail.com>
2020-05-07 16:42:57 +0530

    Custom GetUser() decorator created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -28,9 +30,9 @@ export class AuthController {
         return this.authService.signIn(authCredentialsDto);
     }
 
-    @Post('/test')
-    @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log(req);
-    }
+    // @Post('/test')
+    // @UseGuards(AuthGuard())
+    // test(@GetUser() user: User) {
+    //     console.log(user);
+    // }
 }
